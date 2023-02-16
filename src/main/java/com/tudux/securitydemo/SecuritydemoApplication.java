283f8a8042987132843de4a9131269c563e01de1
AlexAR <moltron_nano@hotmail.com>
2023-02-16 15:01:25 -0600

    feat: initial setup for security spring demo

---
+++ src/main/java/com/tudux/securitydemo/SecuritydemoApplication.java
@@ -0,0 +1,13 @@
+package com.tudux.securitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritydemoApplication.class, args);
+	}
+
+}
