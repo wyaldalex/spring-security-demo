57c34506cc0a39a2488888c8abe6c9bebf41763d
AlexAR <moltron_nano@hotmail.com>
2023-02-16 16:17:33 -0600

    feat: add spring security annotations

---
+++ src/main/java/com/tudux/securitydemo/user/Role.java
@@ -0,0 +1,8 @@
+package com.tudux.securitydemo.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+
+}
--- src/main/java/com/tudux/securitydemo/user/User.java
+++ src/main/java/com/tudux/securitydemo/user/User.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
 @Builder
@@ -12,7 +16,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "jwt_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
@@ -22,4 +26,41 @@ public class User {
     private String email;
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
